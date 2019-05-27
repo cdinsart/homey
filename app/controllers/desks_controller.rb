7cdd1f6515e98709a0b7ea5dede889f4be70f347
Claire Dinsart <cdinsart@gmail.com>
2019-05-27 17:16:12 +0200

    created the controller for desks

---
+++ app/controllers/desks_controller.rb
@@ -0,0 +1,25 @@
+class DesksController < ApplicationController
+  def my_desks
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'desks/my_desks'
+  get 'desks/index'
+  get 'desks/show'
+  get 'desks/new'
+  get 'desks/create'
+  get 'desks/edit'
+  get 'desks/update'
+  get 'desks/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
