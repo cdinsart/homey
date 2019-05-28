c474b678d76613861dd9bddbfbf1cd2123677f19
Claire Dinsart <cdinsart@gmail.com>
2019-05-28 12:56:56 +0200

    deleting nested attributes

--- app/models/desk.rb
+++ app/models/desk.rb
@@ -2,8 +2,7 @@ class Desk < ApplicationRecord
   belongs_to :user
   has_many :desk_features, dependent: :destroy
   has_many :features, through: :desk_features
-  # nested attributes
-  accepts_nested_attributes_for :desk_features
+
   # basic validation
   validates :title, presence: true
   validates :address, presence: true
