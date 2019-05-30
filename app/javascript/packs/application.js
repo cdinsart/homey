b92762347e630ad0152c105e438af5b81cc09fe6
Florin Diconescu <florin.diconescu@gmail.com>
2019-05-30 16:24:06 +0200

    geolocoding works

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,11 @@ import { toggleTimeSlot } from 'components/booking';
 import Typed from 'typed.js';
 import { initTyped } from '../plugins/init_typed';
 
+import { initAutocomplete } from '../plugins/init_autocomplete';
+
 initTyped();
+
+initAutocomplete();
 
 toggleTimeSlot()
 flatpickr(".datepicker", {
--- app/models/desk.rb
+++ app/models/desk.rb
@@ -1,4 +1,5 @@
 class Desk < ApplicationRecord
+  geocoded_by :address
   belongs_to :user
   has_many :desk_features, dependent: :destroy
   has_many :features, through: :desk_features
@@ -9,5 +10,6 @@ class Desk < ApplicationRecord
   validates :title, presence: true
   validates :address, presence: true
   validates :price, presence: true
-  validates :description, presence: true#, length: { minimum: 10 }
+  validates :description, presence: true # , length: { minimum: 10 }
+  after_validation :geocode, if: :will_save_change_to_address?
 end
