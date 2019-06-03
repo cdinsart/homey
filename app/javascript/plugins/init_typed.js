19278ced4baf8d61d9a922c918586606d4c0a940
David Soutoul <david.soutoul@gmail.com>
2019-06-03 14:26:38 +0200

    finished first version homepage

--- app/javascript/plugins/init_typed.js
+++ app/javascript/plugins/init_typed.js
@@ -1,8 +1,8 @@
 function initTyped() {
   if (document.querySelector('.homepage-typed')) {
     var options = {
-      strings: ["Be Productive Anywhere", "Rent Your Desk For A Day"],
-      typeSpeed: 80,
+      strings: ["Barcelona", "London", "Berlin", "Amsterdam", "Brussels", "Paris", "Bali", "Sydney", "San Diego"],
+      typeSpeed: 100,
       loop: true
     }
     var typed = new Typed('.homepage-typed', options)
