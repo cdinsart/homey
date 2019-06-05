db22e43b1d85fa80df5f369e2eb1f33b560f201e
Florin Diconescu <florin.diconescu@gmail.com>
2019-06-05 11:32:34 +0200

    added notification for incoming bookings

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,16 @@ class User < ApplicationRecord
   has_many :desks
   has_many :bookings
   has_many :reviews
-  # def desks
-  #   @desks = Desk.where(user_id: self.id)
-  # end
+
+  def notify
+    notification = 0
+    if self.desks.any?
+      self.desks.each { |desk|
+        if desk.bookings.any?
+          desk.bookings.each { |booking| notification += 1 if booking.status == 'pending' }
+        end
+      }
+    end
+    return notification
+  end
 end
