153e8b4c51d4850522adf7a534089f851a937287
Claire Dinsart <cdinsart@gmail.com>
2019-06-06 15:14:07 +0200

    trying to implement some js on the review button

--- app/javascript/components/sweetalert.js
+++ app/javascript/components/sweetalert.js
@@ -4,6 +4,7 @@ import Swal from 'sweetalert2'
 const initNotifications = () => {
   successAndRedirect();
   areYouSureNotification();
+  leaveReview();
 }
 
 
@@ -52,3 +53,32 @@ const confirmed = (result, btn) => {
 }
 
 export { initNotifications };
+
+// ------------------------------------------------------------
+
+const leaveReview = () => {
+  const btn = document.querySelector('.btn-review')
+
+  if (btn) {
+    btn.addEventListener('click', (e) => {
+      e.preventDefault();
+      Swal.fire({
+        type: 'question',
+        input: 'textarea',
+        inputPlaceholder: 'Type your message here...',
+        title: 'Give a rating from 1 to 5',
+        input: 'range',
+        inputAttributes: {
+          min: 1,
+          max: 5,
+          step: 1
+        },
+        inputValue: 5,
+        position: 'center',
+        showConfirmButton: true,
+        confirmButtonText: "Leave a review",
+        confirmButtonColor: "#28CC9E",
+      })
+    })
+  }
+}
