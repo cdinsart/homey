f99f16c344ea2fc7581dee076830dd259425c5af
Florin Diconescu <florin.diconescu@gmail.com>
2019-06-06 12:37:03 +0200

    listing first 4 desks on the home page grid

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @desks = Desk.take(4)
+    @desks = Desk.first(4)
   end
 end
